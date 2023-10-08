aedc5d41e6c2b953a8b741e492806a84f0db1d56
TrannqGCC210042 <tranqgcc210042@fpt.edu.vn>
2023-10-08 21:12:44 +0700

    complete identity framework

--- BusinessObjects/AppUser.cs
+++ BusinessObjects/AppUser.cs
@@ -10,8 +10,9 @@ namespace BusinessObjects
 {
 	public class AppUser :  IdentityUser
 	{
-		public string FullName { get; set; }
-		public bool Gender { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public bool? Gender { get; set; }
 		public string Address { get; set; }
 		public string? GoogleId { get; set; }
 		public string? FacebookId { get; set; }
